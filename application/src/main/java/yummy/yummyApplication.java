086eff40831ea7ec66cbe951979a33b6d6df600c
jinhosoon <jinho7329@gmail.com>
2025-01-14 17:47:15 +0900

    init: project setting

---
+++ application/src/main/java/yummy/yummyApplication.java
@@ -0,0 +1,11 @@
+package yummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class yummyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(yummyApplication.class, args);
+    }
+}
