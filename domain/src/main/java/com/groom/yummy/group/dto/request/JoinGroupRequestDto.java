853cfaf8eb407d837e65b865f35f27e8027ba304
makernaru-test00 <qweqwerty12321@gmail.com>
2025-01-20 14:45:08 +0900

    feat: 소모임 관련 Dto 추가

---
+++ domain/src/main/java/com/groom/yummy/group/dto/request/JoinGroupRequestDto.java
@@ -0,0 +1,13 @@
+package com.groom.yummy.group.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JoinGroupRequestDto {
+    private Long userId;
+}
+
