710b11363170c4a61ee0c40135f02e85566f5b94
sukangpunch <popora99@naver.com>
2025-01-22 13:16:58 +0900

    refactor: ìêGroup, reply 에 ìErrorCode 를 구현하고 예외 처리를 추ê수정합니다

--- domain/src/main/java/com/groom/yummy/exception/GroupErrorCode.java
+++ domain/src/main/java/com/groom/yummy/exception/GroupErrorCode.java
@@ -1,9 +1,12 @@
 package com.groom.yummy.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-public enum GroupErrorCode {
-
+@Getter
+public enum GroupErrorCode implements ErrorCode{
+    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "그룹이 존재하지 않습니다."),
+    GROUP_PARTICIPATION_FULL(HttpStatus.NOT_FOUND, "참가 인원이 가득 찼습니다.")
     ;
     private final HttpStatus code;
     private final String message;
--- domain/src/main/java/com/groom/yummy/exception/StoreErrorCode.java
+++ domain/src/main/java/com/groom/yummy/exception/StoreErrorCode.java
@@ -4,13 +4,13 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum ReplyErrorCode implements ErrorCode{
-    REPLY_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."),
+public enum StoreErrorCode implements ErrorCode{
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "가게가 존재하지 않습니다."),
     ;
     private final HttpStatus code;
     private final String message;
 
-    ReplyErrorCode(HttpStatus code, String message) {
+    StoreErrorCode(HttpStatus code, String message) {
         this.code = code;
         this.message = message;
     }
